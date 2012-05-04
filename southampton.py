67aeaf6d7e67a35563eef52928fdcdb0607b9044
KyoungRok Jang <astray0924@gmail.com>
2012-05-04 11:34:13 +0900

    sdf

--- southampton.py
+++ southampton.py
@@ -4,9 +4,7 @@ import sys
 import json
 import urlparse
 
-base_path = './data/'
-
-def do_stats():
+def do_stats(base_path):
 	prot_stat = dict()
 	geo_stat = dict()
 #	created_stat = dict()
@@ -75,10 +73,70 @@ def do_stats():
 	
 	return result
 		
-if __name__ == '__main__':
-	result = do_stats();
+def do_stats2(base_path):
+	default_profile_stat = dict()
+	protected_user_stat = dict()
+	protected_user_list = list()
+	user_stat = 0
 	
-	result_file = open('result', 'w')
+	result = dict()
+	
+	for filename in os.listdir(base_path):
+		print filename
+		
+		f = open(base_path + filename)
+		
+		while True:
+			header = f.readline()
+			if (header == ""):
+				break
+			content = f.readline()
+			
+			# parse the header
+#			print "header: " + header.strip()
+			temp = header.split('###')
+			temp = temp[2].split(' ')
+			uid = temp[2]							# uid
+			
+			# parse the json
+			data = eval(content)				# content in dictionary format
+			
+			protected = unicode(data['protected'])
+			try:
+				default_img = unicode(data['default_profile_image'])
+			except KeyError:
+				default_img = True
+			
+			# analyze protected
+			if not protected_user_stat.has_key(protected):
+				protected_user_stat[protected] = 0
+			protected_user_stat[protected] += 1
+			
+			# save the uid of protected user
+			if protected:
+				protected_user_list.append(uid)
+			
+			# analyze default profile image
+			if not default_profile_stat.has_key(default_img):
+				default_profile_stat[default_img] = 0
+			default_profile_stat[default_img] += 1
+			
+			user_stat += 1
+			if (user_stat % 50000) == 0:
+				print "processed %d th user" % user_stat
+			
+		result['protected_stat'] = protected_user_stat
+		result['protected_list'] = protected_user_list
+		result['default_profile_stat'] = default_profile_stat
+		result['user_count'] = user_stat
+		
+		return result
+
+if __name__ == '__main__':
+	base_path = './temp/'
+
+	result = do_stats2(base_path);
+	result_file = open('result_2nd', 'w')
 	result_file.write(str(result))
 	result_file.close()
 	
